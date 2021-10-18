0ff9b7e2c2234005df02d74c46c0220930e977f3
Yannis Lagrosa <65159359+Lagrossa@users.noreply.github.com>
2021-10-18 11:57:06 -0500

    Uploaded updated files.
    The addition method is completely functional.

--- testingbinary/src/testing.java
+++ testingbinary/src/testing.java
@@ -1,34 +1,37 @@
 public class testing {
 
     public static void main(String[] args){
-     for(int x = 0; x <=100; x++){
-         for(int y = 0; y<=100; y++) {
+        /* for(int x = 0; x <=10000; x++){
+         for(int y = 0; y<=10000; y++) {
             if(binaryAdd(x,y) != x+y){
                 System.out.println("There is an error at..." + x + ", " + y);
                 System.out.println("Expected output: " + (x+y) + "\nCurrent output: " + binaryAdd(x,y));
             }
          }
-     }
+     }*/ //Used to test all values.
+
+        System.out.println(binaryAdd(-1,-5));
+    }
+    static int getAnd(int n1, int n2){
+        return (n1&n2) << 1;
+    }
+
+    static int getXor(int n1, int n2){
+        return (n1^n2);
     }
 
     static int binaryAdd(int number1, int number2){
      int sum;
-     int and1, and2, xor1, xor2, xor3, and3, xor4, and4, xor5, and5, xor6, and6, xor7, and7;
-         and1 = (number1 & number2) << 1;
-         xor1 = (number1 ^ number2);
-         and2 = (xor1 & and1) << 1;
-         xor2 = (xor1 ^ and1);
-         and3 = (xor2 & and2) << 1;
-         xor3 = (xor2 ^ and2);
-         and4 = (xor3 & and3) << 1;
-         xor4 = (xor3 ^ and3);
-         and5 = (xor4 & and4) << 1;
-         xor5 = (xor4 ^ and4);
-         and6 = (xor5 & and5) << 1;
-         xor6 = (xor5 ^ and5);
-         and7 = (xor6 & and6) << 1;
-         xor7 = (xor6 ^ and6);
-     sum = (xor7|and7);
+     int and1 = 0, xor1 = 0;
+     int num1 = number1;
+     int num2 = number2;
+     for(int x = 0; x<40; x++){ //There is a performance issue with this, perhaps I should optimize based off of the value of the largest number?
+         and1 = getAnd(num1, num2);
+         xor1 = getXor(num1, num2);
+         num1 = and1;
+         num2 = xor1;
+     }
+     sum = (xor1|and1);
      return sum;
     }
 }

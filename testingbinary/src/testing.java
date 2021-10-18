c0b8988f5daa22855f7f99e05dece50058823617
Yannis Lagrosa <65159359+Lagrossa@users.noreply.github.com>
2021-10-18 12:26:16 -0500

    Uploaded updated version of code.

--- testingbinary/src/testing.java
+++ testingbinary/src/testing.java
@@ -10,7 +10,7 @@ public class testing {
          }
      }*/ //Used to test all values.
 
-        System.out.println(binaryAdd(-1,-5));
+        System.out.println(binaryAdd(-100,500));
     }
     static int getAnd(int n1, int n2){
         return (n1&n2) << 1;
@@ -21,17 +21,17 @@ public class testing {
     }
 
     static int binaryAdd(int number1, int number2){
-     int sum;
+     int binaryLoops = 30; //If you change this variable, make sure the sum of the two numbers is less than 2^(binaryLoops+1)-1, otherwise issues may arise.
      int and1 = 0, xor1 = 0;
      int num1 = number1;
      int num2 = number2;
-     for(int x = 0; x<40; x++){ //There is a performance issue with this, perhaps I should optimize based off of the value of the largest number?
+     for(int x = 0; x<binaryLoops; x++){ //There is a performance issue with this, perhaps I should optimize based off of the value of the largest number?
          and1 = getAnd(num1, num2);
          xor1 = getXor(num1, num2);
          num1 = and1;
          num2 = xor1;
      }
-     sum = (xor1|and1);
+     int sum = (xor1|and1);
      return sum;
     }
 }
